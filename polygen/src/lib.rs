bb5cfe40463ece11da21d7da3cde36eb65b5eda9
Mick van Gelderen <mickvangelderen@gmail.com>
2019-03-31 17:41:02 +0200

    Move geometry generation into its own binary

--- polygen/src/lib.rs
+++ polygen/src/lib.rs
@@ -1,7 +1,10 @@
+// NOTE: A lot of things are private to this module because I want to retain
+// flexibility in the implementations.
+
 use std::f32::consts::*;
 
 type Quad = [u32; 4];
-// type Tri = [u32; 3];
+type Tri = [u32; 3];
 
 #[derive(Debug, Clone, Copy)]
 enum Basis {
@@ -175,18 +178,64 @@ unsafe fn plane_index(n: u32, basis: Basis, i3: u32, i2: u32, i1: Face) -> u32 {
     }
 }
 
-pub fn generate_cube_quads(subdivisions: u32) -> Vec<Quad> {
+trait Array<T>: Sized {
+    const LENGTH: usize;
+
+    fn as_slice(&self) -> &[T];
+}
+
+macro_rules! impl_arrays {
+    ($($N: expr,)*) => {
+        $(
+            impl<T> Array<T> for [T; $N] {
+                const LENGTH: usize = $N;
+
+                fn as_slice(&self) -> &[T] {
+                    self
+                }
+            }
+        )*
+    }
+}
+
+impl_arrays!(1, 2,);
+
+trait FromQuad<T>: Sized {
+    type Polygons: Array<Self>;
+
+    fn from_quad(vertices: [T; 4]) -> Self::Polygons;
+}
+
+impl<T> FromQuad<T> for [T; 4] {
+    type Polygons = [Self; 1];
+
+    fn from_quad(quad: [T; 4]) -> Self::Polygons {
+        [quad]
+    }
+}
+
+impl<T: Copy> FromQuad<T> for [T; 3] {
+    type Polygons = [Self; 2];
+
+    fn from_quad(quad: [T; 4]) -> Self::Polygons {
+        let [a, b, c, d] = quad;
+        [[a, b, c], [c, d, a]]
+    }
+}
+
+
+fn cube_polygons<P: FromQuad<u32> + Clone>(subdivisions: u32) -> Vec<P> {
     // Safe because we promise i3, i2 and i1 are always less than or equal to n + 1.
     unsafe {
         let n = subdivisions;
 
-        let mut faces: Vec<Quad> = Vec::with_capacity((6 * (n + 1) * (n + 1)) as usize);
+        let mut faces: Vec<P> = Vec::with_capacity(P::Polygons::LENGTH * (6 * (n + 1) * (n + 1)) as usize);
 
         for &basis in BASES.into_iter() {
             for &i1 in FACES.into_iter() {
                 for i3 in 0..=n {
                     for i2 in 0..=n {
-                        faces.push(match i1 {
+                        faces.extend_from_slice(P::from_quad(match i1 {
                             Face::Negative => [
                                 plane_index(n, basis, i3, i2, i1),
                                 plane_index(n, basis, i3 + 1, i2, i1),
@@ -199,7 +248,7 @@ pub fn generate_cube_quads(subdivisions: u32) -> Vec<Quad> {
                                 plane_index(n, basis, i3 + 1, i2 + 1, i1),
                                 plane_index(n, basis, i3 + 1, i2, i1),
                             ],
-                        })
+                        }).as_slice());
                     }
                 }
             }
@@ -209,7 +258,15 @@ pub fn generate_cube_quads(subdivisions: u32) -> Vec<Quad> {
     }
 }
 
-pub fn generate_cube_vertices(radius: f32, subdivisions: u32) -> Vec<[f32; 3]> {
+pub fn cube_tris(subdivisions: u32) -> Vec<Tri> {
+    cube_polygons(subdivisions)
+}
+
+pub fn cube_quads(subdivisions: u32) -> Vec<Quad> {
+    cube_polygons(subdivisions)
+}
+
+pub fn cube_vertices(radius: f32, subdivisions: u32) -> Vec<[f32; 3]> {
     let n = subdivisions;
 
     let mut vertices = Vec::with_capacity((8 + 12 * n + 6 * n * n) as usize);
@@ -263,7 +320,7 @@ pub fn generate_cube_vertices(radius: f32, subdivisions: u32) -> Vec<[f32; 3]> {
     vertices
 }
 
-pub fn generate_cubic_sphere_vertices(radius: f32, n: u32) -> Vec<[f32; 3]> {
+pub fn cubic_sphere_vertices(radius: f32, n: u32) -> Vec<[f32; 3]> {
     let frac_1_sqrt_3 = f32::sqrt(1.0 / 3.0);
     let face_range = (-FRAC_PI_4, FRAC_PI_4);
 
@@ -330,152 +387,3 @@ pub fn generate_cubic_sphere_vertices(radius: f32, n: u32) -> Vec<[f32; 3]> {
 
     vertices
 }
-
-// #[derive(Debug)]
-// enum Poly {
-//     None,
-//     Quad(Quad),
-//     Tri(Tri),
-// }
-
-// trait FindOrPushGetIndex<T> {
-//     fn find_or_push_get_index(&mut self, value: T) -> usize;
-// }
-
-// impl<T: Copy + PartialEq> FindOrPushGetIndex<T> for Vec<T> {
-//     fn find_or_push_get_index(&mut self, value: T) -> usize {
-//         self.iter()
-//             .position(|&item| item == value)
-//             .unwrap_or_else(|| {
-//                 let position = self.len();
-//                 self.push(value);
-//                 position
-//             })
-//     }
-// }
-
-// trait PushGetIndex<T> {
-//     fn push_get_index(&mut self, value: T) -> usize;
-// }
-
-// impl<T> PushGetIndex<T> for Vec<T> {
-//     fn push_get_index(&mut self, value: T) -> usize {
-//         let index = self.len();
-//         self.push(value);
-//         index
-//     }
-// }
-
-// pub fn generate_tetra_sphere(
-//     scale: f32,
-//     subdivisions: u32,
-// ) -> (Vec<[f32; 3]>, Vec<Tri>, Vec<u32>) {
-//     let mut positions = vec![
-//         Vector3::new(0.0, scale, 0.0),
-//         Vector3::new(0.0, -(1.0 / 3.0) * scale, f32::sqrt(8.0 / 9.0) * scale),
-//         Vector3::new(
-//             -f32::sqrt(2.0 / 3.0) * scale,
-//             -(1.0 / 3.0) * scale,
-//             -f32::sqrt(2.0 / 9.0) * scale,
-//         ),
-//         Vector3::new(
-//             f32::sqrt(2.0 / 3.0) * scale,
-//             -(1.0 / 3.0) * scale,
-//             -f32::sqrt(2.0 / 9.0) * scale,
-//         ),
-//     ];
-
-//     // Upper bound.
-//     let mut triangles: Vec<Tri> = Vec::with_capacity(4 * 3usize.pow(subdivisions + 1));
-//     triangles.extend([[0, 1, 2], [2, 3, 0], [0, 3, 1], [1, 3, 2]].into_iter());
-
-//     let mut objects: Vec<u32> = Vec::with_capacity(subdivisions as usize + 2);
-//     objects.extend([0, triangles.len() as u32].into_iter());
-
-//     for subdivision in 0..subdivisions {
-//         let triangle_start = objects[subdivision as usize] as usize;
-//         let triangle_end = objects[subdivision as usize + 1] as usize;
-
-//         // println!("triangles {}..{}", triangle_start, triangle_end);
-//         // for i in triangle_start..triangle_end {
-//         //     println!("{:02}: {:?}", i, triangles[i]);
-//         // }
-
-//         for t1_idx in triangle_start..triangle_end {
-//             let t1 = triangles[t1_idx];
-//             let t1_center =
-//                 (positions[t1[0] as usize] + positions[t1[1] as usize] + positions[t1[2] as usize])
-//                     .normalize_to(scale);
-
-//             let mut poly = Poly::Tri(t1);
-
-//             let mut largest_diff = 0.0;
-
-//             for t2_idx in triangle_start..triangle_end {
-//                 if t1_idx == t2_idx {
-//                     continue;
-//                 }
-//                 let t2 = triangles[t2_idx];
-//                 let t2_center = (positions[t2[0] as usize]
-//                     + positions[t2[1] as usize]
-//                     + positions[t2[2] as usize])
-//                     .normalize_to(scale);
-
-//                 for i in 0..3 {
-//                     let next_i = (i + 1) % 3;
-//                     for j in 0..3 {
-//                         let next_j = (j + 1) % 3;
-//                         if t1[i] == t2[next_j] && t1[next_i] == t2[j] {
-//                             let shared_center =
-//                                 positions[t1[i] as usize] + positions[t1[next_i] as usize];
-//                             let cross_center = t1_center + t2_center;
-//                             let diff = cross_center.magnitude2() - shared_center.magnitude2();
-//                             if diff > largest_diff {
-//                                 largest_diff = diff;
-//                                 let prev_i = (i + 2) % 3;
-//                                 let prev_j = (j + 2) % 3;
-//                                 if t1_idx < t2_idx {
-//                                     poly = Poly::Quad([t1[prev_i], t1[i], t2[prev_j], t2[j]]);
-//                                 } else {
-//                                     poly = Poly::None;
-//                                 }
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-
-//             match poly {
-//                 Poly::None => {}
-//                 Poly::Tri([a, b, c]) => {
-//                     let d = positions.len() as u32;
-//                     positions.push(
-//                         (positions[a as usize] + positions[b as usize] + positions[c as usize])
-//                             .normalize_to(scale),
-//                     );
-//                     triangles.push([a, b, d]);
-//                     triangles.push([b, c, d]);
-//                     triangles.push([c, a, d]);
-//                 }
-//                 Poly::Quad([a, b, c, d]) => {
-//                     let e = positions.len() as u32;
-//                     positions.push(
-//                         (positions[a as usize]
-//                             + positions[b as usize]
-//                             + positions[c as usize]
-//                             + positions[d as usize])
-//                             .normalize_to(scale),
-//                     );
-//                     triangles.push([a, b, e]);
-//                     triangles.push([b, c, e]);
-//                     triangles.push([c, d, e]);
-//                     triangles.push([d, a, e]);
-//                 }
-//             }
-//         }
-
-//         objects.push(triangles.len() as u32);
-//     }
-
-//     (positions, triangles, objects)
-// }
