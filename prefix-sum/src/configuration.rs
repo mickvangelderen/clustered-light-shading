d1c8f67e02c1a48e3fcdb0bb327b2e38b286775f
Mick van Gelderen <mickvangelderen@gmail.com>
2019-07-07 21:06:50 +0200

    WIP Undeterministic compute shader results
    Can't figure out why at some point the calculations are off.

--- prefix-sum/src/configuration.rs
+++ prefix-sum/src/configuration.rs
@@ -3,7 +3,15 @@ pub struct Root {
     pub local_x: u32,
     pub local_y: u32,
     pub local_z: u32,
-    pub item_count: usize,
+    pub item_count: u32,
+    pub input: Input,
+    pub iterations: u32,
+}
+
+impl Root {
+    pub fn local_xyz(&self) -> u32 {
+        self.local_x * self.local_y * self.local_z
+    }
 }
 
 pub fn read(configuration_path: impl AsRef<std::path::Path>) -> Root {
@@ -19,4 +27,10 @@ pub fn read(configuration_path: impl AsRef<std::path::Path>) -> Root {
             panic!("Failed to read configuration file {:?}: {}.", configuration_path, err)
         }
     }
+}
+
+#[derive(serde::Deserialize, Debug, Copy, Clone)]
+pub struct Input {
+    pub min: u32,
+    pub max: u32,
 }
