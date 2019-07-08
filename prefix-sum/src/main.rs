e1d81191e579f919989c54660330e2a702531b97
Mick van Gelderen <mickvangelderen@gmail.com>
2019-07-08 19:53:47 +0200

    Working prefix sum minus index violations

--- prefix-sum/src/main.rs
+++ prefix-sum/src/main.rs
@@ -1,25 +1,42 @@
-mod configuration;
-mod convert;
-mod gl_ext;
-mod profiling;
+pub mod configuration;
+pub mod convert;
+pub mod gl_ext;
+pub mod profiling;
 
 pub(crate) use convert::*;
 pub(crate) use gl_ext::*;
 pub(crate) use gl_typed as gl;
 pub(crate) use profiling::*;
 pub(crate) use rand::prelude::*;
+pub(crate) use std::path::Path;
+
+fn u32_div_ceil(a: u32, b: u32) -> u32 {
+    a / b
+        + match a % b {
+            0 => 0,
+            _ => 1,
+        }
+}
 
 fn main() {
     let cfg = configuration::read("prefix-sum/configuration.toml");
     println!("{:#?}", cfg);
-    dbg!(cfg.block_size());
-    dbg!(cfg.block_count());
 
-    assert_eq!(0, cfg.item_count % cfg.block_size());
+    let N = cfg.item_count;
+    let L = cfg.local_xyz() * cfg.local_xyz();
+    let C = u32_div_ceil(N, L);
+    let B = C * cfg.local_xyz();
+    let D = u32_div_ceil(N, B);
+
+    dbg!(N);
+    dbg!(L);
+    dbg!(C);
+    dbg!(B);
+    dbg!(D);
 
     let event_loop = glutin::event_loop::EventLoop::new();
     let context = glutin::ContextBuilder::new()
-        .build_headless(&event_loop, glutin::dpi::PhysicalSize::new(1920.0, 1080.0))
+        .build_windowed(glutin::window::WindowBuilder::new().with_visible(false), &event_loop)
         .unwrap();
 
     let context = unsafe { context.make_current().unwrap() };
@@ -50,35 +67,30 @@ fn main() {
         .take(cfg.iterations as usize)
         .collect();
 
-    let (shader_down, program_down) = prefix_sum_program(&gl, &cfg, Pass::Down);
-    let (shader_up, program_up) = prefix_sum_program(&gl, &cfg, Pass::Up);
+    let (_s0, ps0) = prefix_sum_program(&gl, &cfg, "resources/ps0.comp");
+    let (_s1, ps1) = prefix_sum_program(&gl, &cfg, "resources/ps1.comp");
+    let (_s2, ps2) = prefix_sum_program(&gl, &cfg, "resources/ps2.comp");
 
     let values: Vec<u32> = {
         let rng = rand::thread_rng();
         let dist = rand::distributions::Uniform::new_inclusive(cfg.input.min, cfg.input.max);
-        rng.sample_iter(dist).take(cfg.item_count as usize).collect()
+        rng.sample_iter(dist).take(N as usize).collect()
     };
-
-    let cpu_result: Vec<u32> = values
-        .chunks_exact(cfg.block_size() as usize)
-        .flat_map(|chunk| {
-            chunk.iter().scan(0, |state, &item| {
-                *state += item;
-                Some(*state)
-            })
-        })
-        .collect();
 
     unsafe {
         let input_buffer = gl.create_buffer();
+        let offset_buffer = gl.create_buffer();
         let output_buffer = gl.create_buffer();
 
-        gl.named_buffer_storage(input_buffer, values.vec_as_bytes(), gl::BufferStorageFlag::empty());
+        let buffer_flags =
+            gl::BufferStorageFlag::DYNAMIC_STORAGE | gl::BufferStorageFlag::READ | gl::BufferStorageFlag::WRITE;
+        gl.named_buffer_storage(input_buffer, values.vec_as_bytes(), buffer_flags);
         gl.named_buffer_storage_reserve(
-            output_buffer,
-            values.vec_as_bytes().len(),
-            gl::BufferStorageFlag::empty(),
+            offset_buffer,
+            std::mem::size_of::<u32>() * cfg.local_xyz() as usize,
+            buffer_flags,
         );
+        gl.named_buffer_storage_reserve(output_buffer, values.vec_as_bytes().len(), buffer_flags);
         gl.clear_named_buffer_sub_data(
             output_buffer,
             gl::R32UI,
@@ -90,7 +102,8 @@ fn main() {
         );
 
         gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 0, input_buffer);
-        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 1, output_buffer);
+        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 1, offset_buffer);
+        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 2, output_buffer);
 
         {
             let rec = total_profiler.record(gl, epoch);
@@ -117,18 +130,20 @@ fn main() {
             for profiler in profilers.iter_mut() {
                 let rec = profiler.record(gl, epoch);
 
-                let pass_0_count = cfg.item_count / cfg.block_size();
-                gl.use_program(*program_up.as_ref());
-                gl.dispatch_compute(pass_0_count, 1, 1);
-                gl.memory_barrier(gl::MemoryBarrierFlag::BUFFER_UPDATE);
+                gl.use_program(*ps0.as_ref());
+                gl.dispatch_compute(D, 1, 1);
+                gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE);
 
-                // let pass_1_count = pass_0_count / cfg.block_size();
-                // gl.use_program(*program_down.as_ref());
-                // gl.dispatch_compute(pass_1_count, 1, 1);
-                // gl.memory_barrier(gl::MemoryBarrierFlag::BUFFER_UPDATE);
+                gl.use_program(*ps1.as_ref());
+                gl.dispatch_compute(1, 1, 1);
+                gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE);
+
+                gl.use_program(*ps2.as_ref());
+                gl.dispatch_compute(D, 1, 1);
+                gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE | gl::MemoryBarrierFlag::BUFFER_UPDATE);
 
                 drop(rec);
-                gl.finish();
+                context.swap_buffers().unwrap();
             }
             drop(rec);
         }
@@ -136,14 +151,24 @@ fn main() {
         println!("querying gpu data...");
 
         // Query computation result.
-        let mut gpu_result: Vec<u32> = std::iter::repeat(0).take(values.len()).collect();
-
         gl.memory_barrier(gl::MemoryBarrierFlag::BUFFER_UPDATE);
+
+        let mut gpu_offsets: Vec<u32> = std::iter::repeat(0).take(cfg.local_xyz() as usize).collect();
+
+        gl.get_named_buffer_sub_data(
+            &offset_buffer,
+            0,
+            gpu_offsets.vec_as_bytes().len(),
+            gpu_offsets.vec_as_bytes_mut(),
+        );
+
+        let mut gpu_values: Vec<u32> = std::iter::repeat(0).take(values.len()).collect();
+
         gl.get_named_buffer_sub_data(
             &output_buffer,
             0,
-            std::mem::size_of_val(&gpu_result[..]),
-            gpu_result.vec_as_bytes_mut(),
+            gpu_values.vec_as_bytes().len(),
+            gpu_values.vec_as_bytes_mut(),
         );
 
         let total = total_profiler.query(gl);
@@ -188,33 +213,32 @@ fn main() {
             .unwrap();
         println!("iteration max {:?} CPU | {:?} GPU", Ns(cpu_max), Ns(gpu_max),);
 
-        let b = cfg.block_size() as usize;
-        for i in 0..cfg.item_count as usize / b {
-            let expected = &cpu_result[i * b..(i + 1) * b];
-            let actual = &gpu_result[i * b..(i + 1) * b];
-            if expected != actual {
-                panic!("block {} expected {:?}, got {:?}", i, expected, actual);
-            }
-        }
+        let cpu_offsets: Vec<u32> = values
+            .chunks(B as usize)
+            .map(|chunk| chunk.iter().sum::<u32>())
+            .scan(0, |state, item| {
+                *state += item;
+                Some(*state)
+            })
+            .collect();
+
+        assert_eq!(D, cpu_offsets.len() as u32);
+
+        assert_eq!(cpu_offsets[0..D as usize], gpu_offsets[0..D as usize]);
+
+        let cpu_values: Vec<u32> = values
+            .iter()
+            .scan(0, |state, &item| {
+                *state += item;
+                Some(*state)
+            })
+            .collect();
+
+        assert_eq!(cpu_values, gpu_values);
     }
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum Pass {
-    Down,
-    Up,
-}
-
-impl Pass {
-    pub fn header(self) -> &'static str {
-        match self {
-            Pass::Down => "#define PASS_DOWN\n",
-            Pass::Up => "#define PASS_UP\n",
-        }
-    }
-}
-
-fn prefix_sum_program(gl: &gl::Gl, cfg: &configuration::Root, pass: Pass) -> (ShaderName, ProgramName) {
+fn prefix_sum_program(gl: &gl::Gl, cfg: &configuration::Root, path: impl AsRef<Path>) -> (ShaderName, ProgramName) {
     let mut shader = ShaderName::new(gl, gl::COMPUTE_SHADER);
     let header = format!(
         r"
@@ -223,27 +247,26 @@ fn prefix_sum_program(gl: &gl::Gl, cfg: &configuration::Root, pass: Pass) -> (Sh
 #define LOCAL_X {}
 #define LOCAL_Y {}
 #define LOCAL_Z {}
+#define LOCAL_XYZ {}
 
 #define ITEM_COUNT {}
-
-{}
 ",
         cfg.local_x,
         cfg.local_y,
         cfg.local_z,
+        cfg.local_xyz(),
         cfg.item_count,
-        pass.header()
     );
-    let source = std::fs::read_to_string("resources/prefix_sum.comp").unwrap();
+    let source = std::fs::read_to_string(path.as_ref()).unwrap();
     shader.compile(gl, &[header.as_str(), source.as_str()]);
     if !shader.is_compiled() {
-        panic!(shader.log(gl));
+        panic!("{}: {}", path.as_ref().display(), shader.log(gl));
     }
     let mut program = ProgramName::new(gl);
     program.attach(gl, &[&shader]);
     program.link(gl);
     if !program.is_linked() {
-        panic!(program.log(gl));
+        panic!("{}: {}", path.as_ref().display(), program.log(gl));
     }
     (shader, program)
 }
