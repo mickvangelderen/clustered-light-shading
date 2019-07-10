fd882b871589b71af990ee6bd31bcf82ca5a9e30
Mick van Gelderen <mickvangelderen@gmail.com>
2019-07-10 12:41:49 +0200

    Refactor prefix sum resources and computation

--- prefix-sum/src/main.rs
+++ prefix-sum/src/main.rs
@@ -82,9 +82,7 @@ fn main() {
         .take(cfg.iterations as usize)
         .collect();
 
-    let (_s0, ps0) = prefix_sum_program(&gl, &cfg, "resources/ps0.comp");
-    let (_s1, ps1) = prefix_sum_program(&gl, &cfg, "resources/ps1.comp");
-    let (_s2, ps2) = prefix_sum_program(&gl, &cfg, "resources/ps2.comp");
+    let mut ps_res = unsafe { PrefixSumResources::new(&gl, cfg.prefix_sum) };
 
     let values: Vec<u32> = {
         let rng = rand::thread_rng();
@@ -94,7 +92,6 @@ fn main() {
 
     unsafe {
         let input_buffer = gl.create_buffer();
-        let offset_buffer = gl.create_buffer();
         let output_buffer = gl.create_buffer();
 
         let buffer_flags =
@@ -116,13 +113,6 @@ fn main() {
             None,
         );
 
-        // Offset buffer (uninitialized).
-        gl.named_buffer_storage_reserve(
-            offset_buffer,
-            std::mem::size_of::<u32>() * cfg.prefix_sum.pass_1_threads as usize,
-            buffer_flags,
-        );
-
         // Output buffer (zeros).
         // Actual implementation can re-use input buffer or leave second buffer undefined.
         gl.named_buffer_storage_reserve(
@@ -139,10 +129,6 @@ fn main() {
             gl::UNSIGNED_INT,
             None,
         );
-
-        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 0, input_buffer);
-        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 1, offset_buffer);
-        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 2, output_buffer);
 
         {
             let rec = total_profiler.record(gl, epoch);
@@ -167,30 +153,20 @@ fn main() {
             // }
 
             for i in 0..profilers.len() {
-                let rec = profilers[i].record(gl, epoch);
+                ps_res.compute(PrefixSumComputeParams {
+                    gl: &gl,
+                    count: cfg.input.count,
+                    input_buffer: &input_buffer,
+                    output_buffer: &output_buffer,
+                    epoch: &epoch,
+                    profilers: PrefixSumProfilers {
+                        total: &mut profilers[i],
+                        pass_0: &mut ps0_profilers[i],
+                        pass_1: &mut ps1_profilers[i],
+                        pass_2: &mut ps2_profilers[i],
+                    },
+                });
 
-                let ps0_rec = ps0_profilers[i].record(gl, epoch);
-                gl.use_program(*ps0.as_ref());
-                gl.uniform_1ui(ITEM_COUNT_LOC, cfg.input.count);
-                gl.dispatch_compute(block_count, 1, 1);
-                drop(ps0_rec);
-                gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE);
-
-                let ps1_rec = ps1_profilers[i].record(gl, epoch);
-                gl.use_program(*ps1.as_ref());
-                gl.uniform_1ui(ITEM_COUNT_LOC, cfg.input.count);
-                gl.dispatch_compute(1, 1, 1);
-                drop(ps1_rec);
-                gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE);
-
-                let ps2_rec = ps2_profilers[i].record(gl, epoch);
-                gl.use_program(*ps2.as_ref());
-                gl.uniform_1ui(ITEM_COUNT_LOC, cfg.input.count);
-                gl.dispatch_compute(block_count, 1, 1);
-                drop(ps2_rec);
-                gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE | gl::MemoryBarrierFlag::BUFFER_UPDATE);
-
-                drop(rec);
                 context.swap_buffers().unwrap();
             }
             drop(rec);
@@ -228,7 +204,7 @@ fn main() {
             .collect();
 
         gl.get_named_buffer_sub_data(
-            &offset_buffer,
+            &ps_res.offset_buffer,
             0,
             gpu_offsets.vec_as_bytes().len(),
             gpu_offsets.vec_as_bytes_mut(),
@@ -260,7 +236,124 @@ fn main() {
     }
 }
 
-fn prefix_sum_program(gl: &gl::Gl, cfg: &configuration::Root, path: impl AsRef<Path>) -> (ShaderName, ProgramName) {
+pub struct PrefixSumResources {
+    pass_0_program: ProgramName,
+    pass_0_shader: ShaderName,
+    pass_1_program: ProgramName,
+    pass_1_shader: ShaderName,
+    pass_2_program: ProgramName,
+    pass_2_shader: ShaderName,
+    offset_buffer: gl::BufferName,
+    cfg: configuration::PrefixSum,
+}
+
+pub struct PrefixSumComputeParams<'a> {
+    gl: &'a gl::Gl,
+    count: u32,
+    input_buffer: &'a gl::BufferName,
+    output_buffer: &'a gl::BufferName,
+    epoch: &'a std::time::Instant,
+    profilers: PrefixSumProfilers<'a>,
+}
+
+pub struct PrefixSumProfilers<'a> {
+    total: &'a mut Profiler,
+    pass_0: &'a mut Profiler,
+    pass_1: &'a mut Profiler,
+    pass_2: &'a mut Profiler,
+}
+
+impl PrefixSumResources {
+    pub unsafe fn new(gl: &gl::Gl, cfg: configuration::PrefixSum) -> Self {
+        let (pass_0_shader, pass_0_program) = prefix_sum_program(&gl, &cfg, "resources/ps0.comp");
+        let (pass_1_shader, pass_1_program) = prefix_sum_program(&gl, &cfg, "resources/ps1.comp");
+        let (pass_2_shader, pass_2_program) = prefix_sum_program(&gl, &cfg, "resources/ps2.comp");
+
+        let offset_buffer = gl.create_buffer();
+        gl.named_buffer_storage_reserve(
+            offset_buffer,
+            std::mem::size_of::<u32>() * cfg.pass_1_threads as usize,
+            // FIXME: Select less conservative.
+            gl::BufferStorageFlag::DYNAMIC_STORAGE | gl::BufferStorageFlag::READ | gl::BufferStorageFlag::WRITE,
+        );
+
+        Self {
+            pass_0_program,
+            pass_0_shader,
+            pass_1_program,
+            pass_1_shader,
+            pass_2_program,
+            pass_2_shader,
+            offset_buffer,
+            cfg,
+        }
+    }
+
+    pub unsafe fn compute(&self, params: PrefixSumComputeParams) {
+        let PrefixSumComputeParams {
+            gl,
+            count,
+            input_buffer,
+            output_buffer,
+            epoch,
+            profilers,
+        } = params;
+
+        let Self {
+            pass_0_program,
+            pass_1_program,
+            pass_2_program,
+            offset_buffer,
+            cfg,
+            ..
+        } = self;
+
+        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 0, *input_buffer);
+        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 1, *offset_buffer);
+        gl.bind_buffer_base(gl::SHADER_STORAGE_BUFFER, 2, *output_buffer);
+
+        // number of chunks per block.
+        let chunks_per_block = u32_div_ceil(count, cfg.pass_0_threads * cfg.pass_1_threads);
+
+        // items per block.
+        let items_per_block = chunks_per_block * cfg.pass_0_threads;
+
+        // number of blocks.
+        let block_count = u32_div_ceil(count, items_per_block);
+
+        let rec = profilers.total.record(gl, epoch);
+
+        let ps0_rec = profilers.pass_0.record(gl, epoch);
+        gl.use_program(*pass_0_program.as_ref());
+        gl.uniform_1ui(ITEM_COUNT_LOC, count);
+        gl.dispatch_compute(block_count, 1, 1);
+        drop(ps0_rec);
+        gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE);
+
+        let ps1_rec = profilers.pass_1.record(gl, epoch);
+        gl.use_program(*pass_1_program.as_ref());
+        gl.uniform_1ui(ITEM_COUNT_LOC, count);
+        gl.dispatch_compute(1, 1, 1);
+        drop(ps1_rec);
+        gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE);
+
+        let ps2_rec = profilers.pass_2.record(gl, epoch);
+        gl.use_program(*pass_2_program.as_ref());
+        gl.uniform_1ui(ITEM_COUNT_LOC, count);
+        gl.dispatch_compute(block_count, 1, 1);
+        drop(ps2_rec);
+        gl.memory_barrier(gl::MemoryBarrierFlag::SHADER_STORAGE | gl::MemoryBarrierFlag::BUFFER_UPDATE);
+
+        drop(rec);
+    }
+}
+
+
+fn prefix_sum_program(
+    gl: &gl::Gl,
+    cfg: &configuration::PrefixSum,
+    path: impl AsRef<Path>,
+) -> (ShaderName, ProgramName) {
     let mut shader = ShaderName::new(gl, gl::COMPUTE_SHADER);
     let header = format!(
         r"
@@ -271,8 +364,8 @@ fn prefix_sum_program(gl: &gl::Gl, cfg: &configuration::Root, path: impl AsRef<P
 
 #define ITEM_COUNT_LOC {}
 ",
-        cfg.prefix_sum.pass_0_threads,
-        cfg.prefix_sum.pass_1_threads,
+        cfg.pass_0_threads,
+        cfg.pass_1_threads,
         ITEM_COUNT_LOC.into_i32(),
     );
     let common = std::fs::read_to_string("resources/ps_common.comp").unwrap();
