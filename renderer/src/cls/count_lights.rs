56381d30040f1c5d7d5a7843dae0e82cdaf27a3c
Mick van Gelderen <mickvangelderen@gmail.com>
2019-07-26 14:23:04 +0200

    Fix count light program uniform locations

--- renderer/src/cls/count_lights.rs
+++ renderer/src/cls/count_lights.rs
@@ -1,8 +1,8 @@
 pub use crate::*;
 
 pub const CLUSTER_DIMS_LOC: gl::UniformLocation = unsafe { gl::UniformLocation::new_unchecked(0) };
-pub const TRANSLATION_LOC: gl::UniformLocation = unsafe { gl::UniformLocation::new_unchecked(1) };
-pub const SCALE_LOC: gl::UniformLocation = unsafe { gl::UniformLocation::new_unchecked(2) };
+pub const SCALE_LOC: gl::UniformLocation = unsafe { gl::UniformLocation::new_unchecked(1) };
+pub const TRANSLATION_LOC: gl::UniformLocation = unsafe { gl::UniformLocation::new_unchecked(2) };
 pub const LIGHT_COUNT_LOC: gl::UniformLocation = unsafe { gl::UniformLocation::new_unchecked(3) };
 
 pub struct CountLightsProgram {
