fe9a56600ef1c60e49315d3e2cfffcbb26c11a02
Mick van Gelderen <mickvangelderen@gmail.com>
2019-07-30 11:50:44 +0200

    Collect profiling samples over time

--- renderer/src/profiling.rs
+++ renderer/src/profiling.rs
@@ -47,14 +47,14 @@ pub struct GpuCpuTimeSpan {
 }
 
 #[derive(Debug)]
-pub struct Profiler {
+pub struct ProfilerTimer {
     begin_query_name: gl::QueryName,
     end_query_name: gl::QueryName,
     cpu_begin: Option<NonZeroU64>,
     cpu_end: Option<NonZeroU64>,
 }
 
-impl Profiler {
+impl ProfilerTimer {
     #[inline]
     pub fn new(gl: &gl::Gl) -> Self {
         Self {
@@ -111,36 +111,157 @@ impl Profiler {
     }
 }
 
-pub struct ProfilerPool {
-    pool: [Profiler; Self::CAPACITY],
+pub struct ProfilerTimerPool {
+    pool: [ProfilerTimer; Self::CAPACITY],
 }
 
-impl ProfilerPool {
+impl ProfilerTimerPool {
     pub const CAPACITY: usize = 3;
 
     pub fn new(gl: &gl::Gl) -> Self {
         Self {
             pool: [
-                Profiler::new(gl),
-                Profiler::new(gl),
-                Profiler::new(gl),
+                ProfilerTimer::new(gl),
+                ProfilerTimer::new(gl),
+                ProfilerTimer::new(gl),
             ]
         }
     }
 }
 
-impl std::ops::Index<u64> for ProfilerPool {
-    type Output = Profiler;
+impl std::ops::Index<u64> for ProfilerTimerPool {
+    type Output = ProfilerTimer;
 
     #[inline]
-    fn index(&self, tick: u64) -> &Self::Output {
-        &self.pool[(tick % Self::CAPACITY as u64) as usize]
+    fn index(&self, frame: u64) -> &Self::Output {
+        &self.pool[(frame % Self::CAPACITY as u64) as usize]
     }
 }
 
-impl std::ops::IndexMut<u64> for ProfilerPool {
+impl std::ops::IndexMut<u64> for ProfilerTimerPool {
     #[inline]
-    fn index_mut(&mut self, tick: u64) -> &mut Self::Output {
-        &mut self.pool[(tick % Self::CAPACITY as u64) as usize]
+    fn index_mut(&mut self, frame: u64) -> &mut Self::Output {
+        &mut self.pool[(frame % Self::CAPACITY as u64) as usize]
+    }
+}
+
+/// Stores profiling samples in a circular buffer. Will clear the buffer when
+/// samples aren't inserted consecutively.
+pub struct ProfilerSampleBuffer {
+    samples: [GpuCpuTimeSpan; Self::CAPACITY],
+    frame: u64,
+    count: usize,
+}
+
+impl ProfilerSampleBuffer {
+    pub const CAPACITY: usize = 8;
+
+    pub fn new() -> Self {
+        Self {
+            samples: [GpuCpuTimeSpan::default(); Self::CAPACITY],
+            frame: 0,
+            count: 0,
+        }
+    }
+
+    pub fn update(&mut self, frame: u64, sample: GpuCpuTimeSpan) {
+        self.samples[(frame % Self::CAPACITY as u64) as usize] = sample;
+        if self.frame + 1 == frame {
+            if self.count < Self::CAPACITY {
+                self.count += 1;
+            }
+        } else {
+            self.count = 1;
+        }
+        self.frame = frame;
+    }
+
+    pub fn stats(&self, frame: u64) -> Option<GpuCpuStats> {
+        if self.frame == frame {
+            let mut iter = self.samples.iter();
+            let first = iter.next();
+            first.map(move |first| {
+                let dg = first.gpu.delta();
+                let dc = first.cpu.delta();
+                let mut stats = iter.fold(GpuCpuStats {
+                    gpu_elapsed_avg: dg,
+                    gpu_elapsed_min: dg,
+                    gpu_elapsed_max: dg,
+                    cpu_elapsed_avg: dc,
+                    cpu_elapsed_min: dc,
+                    cpu_elapsed_max: dc,
+                }, |mut stats, item| {
+                    {
+                        let dg = item.gpu.delta();
+                        stats.gpu_elapsed_avg += dg;
+                        if dg < stats.gpu_elapsed_min {
+                            stats.gpu_elapsed_min = dg;
+                        }
+                        if dg > stats.gpu_elapsed_max {
+                            stats.gpu_elapsed_max = dg;
+                        }
+                    }
+                    {
+                        let dc = item.cpu.delta();
+                        stats.cpu_elapsed_avg += dc;
+                        if dc < stats.cpu_elapsed_min {
+                            stats.cpu_elapsed_min = dc;
+                        }
+                        if dc > stats.cpu_elapsed_max {
+                            stats.cpu_elapsed_max = dc;
+                        }
+                    }
+                    stats
+                });
+
+                stats.gpu_elapsed_avg /= Self::CAPACITY as u64;
+                stats.cpu_elapsed_avg /= Self::CAPACITY as u64;
+
+                stats
+            })
+        } else {
+            None
+        }
+    }
+}
+
+pub struct GpuCpuStats {
+    pub gpu_elapsed_avg: u64,
+    pub gpu_elapsed_min: u64,
+    pub gpu_elapsed_max: u64,
+    pub cpu_elapsed_avg: u64,
+    pub cpu_elapsed_min: u64,
+    pub cpu_elapsed_max: u64,
+}
+
+pub struct Profiler {
+    timers: ProfilerTimerPool,
+    samples: ProfilerSampleBuffer,
+}
+
+impl Profiler {
+    pub fn new(gl: &gl::Gl) -> Self {
+        Self {
+            timers: ProfilerTimerPool::new(gl),
+            samples: ProfilerSampleBuffer::new(),
+        }
+    }
+
+    pub fn start(&mut self, gl: &gl::Gl, frame: u64, epoch: Instant) {
+        let timer = &mut self.timers[frame];
+        let sample = timer.read(gl);
+        if let Some(sample) = sample {
+            self.samples.update(frame, sample);
+        }
+        timer.start(&gl, epoch);
+    }
+
+    pub fn stop(&mut self, gl: &gl::Gl, frame: u64, epoch: Instant) {
+        let timer = &mut self.timers[frame];
+        timer.stop(&gl, epoch);
+    }
+
+    pub fn stats(&self, frame: u64) -> Option<GpuCpuStats> {
+        self.samples.stats(frame)
     }
 }
