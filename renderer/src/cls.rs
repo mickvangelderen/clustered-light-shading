eb19cb6e98c7ba2b919ca7794135340c7be0a5e1
Mick van Gelderen <mickvangelderen@gmail.com>
2019-08-29 15:23:50 +0200

    WIP Seem to have lights per cluster again

--- renderer/src/cls.rs
+++ renderer/src/cls.rs
@@ -4,6 +4,9 @@ pub mod cluster_camera_resources;
 pub mod cluster_renderer;
 pub mod cluster_resources;
 pub mod count_lights;
+pub mod cluster_space_buffer;
 
 pub use cluster_camera_resources::*;
 pub use cluster_resources::*;
+pub use cluster_space_buffer::{ClusterSpaceBuffer};
+
