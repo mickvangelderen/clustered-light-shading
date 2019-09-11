5bdd8bbb327fa7e634191f1b0e432362d0a97aa2
Mick van Gelderen <mickvangelderen@gmail.com>
2019-09-11 21:43:01 +0200

    Sweet got some plots!

--- analysis/log.py
+++ analysis/log.py
@@ -1,19 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-raw_data = np.reshape(np.fromfile('../log/log.bin', dtype='uint64'), (-1, 4));
-print(np.shape(raw_data));
-diff_data = np.diff(raw_data);
-print(np.shape(diff_data));
+blob = np.fromfile('../samples.bin', dtype='uint64')
 
-# fig, axes = plt.subplots(3, 1, sharex=True, sharey=True);
-# for i in range(0, 3):
-    # axe = axes[i];
-    # axe.plot(diff_data[:, i]);
+run_count = blob[0]
+frame_count = blob[1]
+sample_count = blob[2]
+field_count = 4
+
+stamps = np.reshape(blob[3:], (run_count, frame_count, sample_count, field_count))
+
+deltas = np.subtract(stamps[:, :, :, [1, 3]], stamps[:, :, :, [0, 2]])
+
+print(np.shape(deltas))
+
+# samples = np.squeeze(np.median(deltas, axis = 0, keepdims = True));
+
+for sample_index in range(0, sample_count):
+    fig, subs = plt.subplots(1, 1, squeeze = False)
+    sub = subs[0, 0]
+    for run_index in range(0, run_count):
+        sub.plot(deltas[run_index, :, sample_index, 0])
     # axe.semilogy();
+    # legend = ['sim', 'pos', 'ren'];
+    # fig.legend(legend);
+    plt.show()
 
-legend = ['sim', 'pos', 'ren'];
-axe = plt.plot(diff_data);
-plt.legend(legend);
 
-plt.show()
