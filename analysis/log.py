3dcbd2e397350d0e8ace2765b038443d62944b6a
Mick van Gelderen <mickvangelderen@gmail.com>
2019-09-17 17:09:33 +0200

    Write median cpu/gpu elapsed plots to disk

--- analysis/log.py
+++ analysis/log.py
@@ -16,7 +16,8 @@ def read_padded_string(f):
         f.read(pad_count)
     return string
 
-with open("../samples.bin", "rb") as f:
+profiling_dir = "../profiling/2019-09-17_16-04-01/"
+with open(profiling_dir + "samples.bin", "rb") as f:
     run_count = read_u64(f)
     frame_count = read_u64(f)
     sample_count = read_u64(f)
@@ -40,18 +41,40 @@ print(np.shape(deltas))
 def ceil_div(n, d):
     return (n + d - 1) // d
 
+
+my_figsize = (20.00, 11.25);
+my_dpi = 96;
+
 row_count = 5
 
-fig, subs = plt.subplots(ceil_div(sample_count, row_count), row_count, squeeze = False, figsize = (20, 20))
+fig, subs = plt.subplots(ceil_div(sample_count, row_count), row_count, sharex = True, sharey = True, squeeze = False, figsize = my_figsize, dpi = my_dpi)
 for sample_index in range(0, sample_count):
     sub = subs[sample_index // row_count, sample_index % row_count]
-    for run_index in range(0, run_count):
-        sub.plot(deltas[run_index, 1:, sample_index, 1])
+    sub.set_xlabel("frame");
+    sub.set_ylim(0, 120000);
+    sub.set_ylabel("ns");
+
+    # Omit the first run. Take all the frames.
+    current_samples = np.transpose(deltas[1:, :, sample_index, 0])
+    sub.plot(current_samples, color = (0, 0, 0, 0.1))
+    sub.plot(np.median(current_samples, axis = 1, keepdims = True))
+
     sub.title.set_text(sample_names[sample_index])
+plt.savefig(profiling_dir + "cpu.png", dpi=my_dpi)
 
-    # axe.semilogy();
-    # fig.legend(legend);
+fig, subs = plt.subplots(ceil_div(sample_count, row_count), row_count, sharex = True, sharey = True, squeeze = False, figsize = my_figsize, dpi = my_dpi)
+for sample_index in range(0, sample_count):
+    sub = subs[sample_index // row_count, sample_index % row_count]
+    sub.set_xlabel("frame");
+    sub.set_ylim(0, 500000);
+    sub.set_ylabel("ns");
 
-    # legend = ['sim', 'pos', 'ren'];
-plt.show()
+    # Omit the first run. Take all the frames.
+    current_samples = np.transpose(deltas[1:, :, sample_index, 1])
+    sub.plot(current_samples, color = (0, 0, 0, 0.1))
+    sub.plot(np.median(current_samples, axis = 1, keepdims = True))
 
+    sub.title.set_text(sample_names[sample_index])
+plt.savefig(profiling_dir + "gpu.png", dpi=my_dpi)
+
+# plt.show()
