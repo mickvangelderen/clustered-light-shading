d4b32b69ef5a7453d218469d50bbaf1191104b0c
Mick van Gelderen <mickvangelderen@gmail.com>
2019-10-22 17:20:42 +0200

    Read models

--- renderer/src/new_resources.rs
+++ renderer/src/new_resources.rs
@@ -1,11 +1,35 @@
-struct Resources {
-    materials: Vec<Material>,
-    geometries: Vec<Geometry>,
-    textures: Vec<Texture>,
-    buffers: Vec<Buffer>,
+pub struct Resources {
+    pub materials: Vec<Material>,
+    pub textures: Vec<Texture>,
+    pub buffers: Vec<Buffer>,
+    pub meshes: Vec<Mesh>,
+    pub instances: Vec<Instance>,
 }
 
-struct Material {
-    diffuse_texture_id: usize,
-    specular_texture_id: usize,
+pub struct Material {
+    pub diffuse_texture_index: usize,
+    pub specular_texture_index: usize,
+}
+
+pub struct Texture {
+    pub path: PathBuf,
+    pub dimensions: Vector2<u32>,
+    pub 
+}
+
+pub struct Mesh {
+    pub buffer_index: usize,
+    pub byte_offset: usize,
+    pub element_count: usize,
+}
+
+pub struct Instance {
+    pub transform: Transform,
+    pub mesh_index: usize,
+}
+
+pub struct Transform {
+    pub pos: Vector3<f32>,
+    pub scale: Vector3<f32>,
+    pub ori: Quaternion<f32>,
 }
