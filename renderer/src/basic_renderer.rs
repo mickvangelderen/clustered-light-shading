b13b4b5381f21d07de94788940b2c709220fb043
Mick van Gelderen <mickvangelderen@gmail.com>
2019-11-08 16:40:27 +0100

    Factor out instance matrix and draw command computation

--- renderer/src/basic_renderer.rs
+++ renderer/src/basic_renderer.rs
@@ -44,99 +44,6 @@ impl Context<'_> {
             ..
         } = *self;
 
-        let scene_file = &self.resources.scene_file;
-
-        #[derive(Debug)]
-        #[repr(C)]
-        pub struct InstanceMatrices {
-            pub pos_from_obj_to_wld: Matrix4<f32>,
-            pub pos_from_obj_to_lgt: Matrix4<f32>,
-            pub nor_from_obj_to_lgt: Matrix4<f32>,
-        }
-
-        let instance_matrices_buffer: Vec<InstanceMatrices> = scene_file
-            .instances
-            .iter()
-            .map(|instance| {
-                let transform = &scene_file.transforms[instance.transform_index as usize];
-
-                let pos_from_obj_to_wld = {
-                    Matrix4::from_translation(transform.translation.into())
-                        * Matrix4::from(Euler {
-                            x: Deg(transform.rotation[0]),
-                            y: Deg(transform.rotation[1]),
-                            z: Deg(transform.rotation[2]),
-                        })
-                        * Matrix4::from_nonuniform_scale(
-                            transform.scaling[0],
-                            transform.scaling[1],
-                            transform.scaling[2],
-                        )
-                };
-
-                let pos_from_obj_to_lgt = pos_from_obj_to_wld;
-
-                let nor_from_obj_to_lgt = pos_from_obj_to_lgt.invert().unwrap().transpose();
-
-                InstanceMatrices {
-                    pos_from_obj_to_wld,
-                    pos_from_obj_to_lgt,
-                    nor_from_obj_to_lgt,
-                }
-            })
-            .collect();
-
-        // Compute prefix sum for draw commands.
-
-        let mut draw_counts: Vec<usize> = scene_file.instances.iter().fold(
-            self.resources.materials.iter().map(|_| 0).collect(),
-            |mut counts, instance| {
-                counts[instance.material_index as usize] += 1;
-                counts
-            },
-        );
-
-        let draw_offsets: Vec<usize> = draw_counts
-            .iter()
-            .scan(0, |offset, &count| {
-                let result = Some(*offset);
-                *offset += count;
-                result
-            })
-            .collect();
-
-        // Clear counts and initialize draw command buffer.
-
-        for draw_count in draw_counts.iter_mut() {
-            *draw_count = 0;
-        }
-
-        let mut draw_commands: Vec<DrawCommand> = std::iter::repeat(DrawCommand {
-            count: 0,
-            prim_count: 0,
-            first_index: 0,
-            base_vertex: 0,
-            base_instance: 0,
-        })
-        .take(scene_file.instances.len())
-        .collect();
-
-        // Write draw commands using the offsets.
-
-        for (instance_index, instance) in scene_file.instances.iter().enumerate() {
-            let material_index = instance.material_index as usize;
-            let mesh_description = &scene_file.mesh_descriptions[instance.mesh_index as usize];
-            let command_index = draw_offsets[material_index] + draw_counts[material_index];
-            draw_commands[command_index] = DrawCommand {
-                count: mesh_description.element_count(),
-                prim_count: 1,
-                first_index: mesh_description.element_offset(),
-                base_vertex: mesh_description.vertex_offset,
-                base_instance: instance_index as u32,
-            };
-            draw_counts[material_index] += 1;
-        }
-
         unsafe {
             basic_renderer.opaque_program.update(&mut rendering_context!(self));
             basic_renderer.masked_program.update(&mut rendering_context!(self));
@@ -184,7 +91,8 @@ impl Context<'_> {
 
                 {
                     let buffer = &mut basic_renderer.instance_matrices_buffer;
-                    let bytes = instance_matrices_buffer.vec_as_bytes();
+                    let data = resources::compute_instance_matrices(&self.resources);
+                    let bytes = data.vec_as_bytes();
                     buffer.ensure_capacity(gl, bytes.len());
                     buffer.write(gl, bytes);
 
@@ -194,6 +102,12 @@ impl Context<'_> {
                         buffer.name(),
                     );
                 }
+
+                let resources::DrawCommandResources {
+                    counts: draw_counts,
+                    offsets: draw_offsets,
+                    buffer: draw_commands,
+                } = resources::compute_draw_commands(&self.resources);
 
                 {
                     let buffer = &mut basic_renderer.draw_commands_buffer;
