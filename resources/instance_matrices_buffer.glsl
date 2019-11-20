740721c257944066e2368ccc6d20f083b797bad2
Mick van Gelderen <mickvangelderen@gmail.com>
2019-11-20 16:18:47 +0100

    WIP Rename instance matrices fields

--- resources/instance_matrices_buffer.glsl
+++ resources/instance_matrices_buffer.glsl
@@ -1,7 +1,8 @@
 struct InstanceMatrices {
-  mat4 pos_from_obj_to_wld;
-  mat4 pos_from_obj_to_lgt;
-  mat4 nor_from_obj_to_lgt;
+  mat4 obj_to_ren_clp; // = wld_to_ren_clp * obj_to_wld
+  mat4 obj_to_clu_clp; // = wld_to_clu_clp * obj_to_wld
+  mat4 obj_to_lgt; // = obj_to_wld
+  mat4 obj_to_lgt_inv_tra; // = transpose(inverse(obj_to_lgt))
 };
 
 layout(std430, binding = INSTANCE_MATRICES_BUFFER_BINDING) buffer InstanceMatricesBuffer {
