e32a91193bb8813cec9cd43753a19bdc0b80da4d
Mick van Gelderen <mickvangelderen@gmail.com>
2019-12-10 17:23:52 +0100

    Plot ortho and persp

--- analysis/profile.py
+++ analysis/profile.py
@@ -2,26 +2,19 @@ import os
 import re
 import toml
 import numpy as np
-from profile_samples import ProfileSamples
 import matplotlib.pyplot as plt
+from profile_samples import ProfileSamples
+import thesis
 
-class Profile:
-    def __init__(self, directory):
-        self.directory = directory
-        self.configuration = toml.load(os.path.join(directory, "configuration.toml"))
-        self.samples = ProfileSamples(directory)
-
-def load_profiles(profiling_directory, regex):
-    with os.scandir(profiling_directory) as it:
-        directories = [os.path.join(profiling_directory, entry.name) for entry in it if entry.is_dir() and regex.match(entry.name)]
-
-    return [Profile(directory) for directory in directories]
-
-profile_dir_regex = re.compile(r"^(suntem|bistro)_[ \d]{7}_(ortho|persp)_[ \d]{4}$");
-profiles = load_profiles("../profiling", profile_dir_regex);
-
-current_profiles = [profile for profile in profiles if
-                    profile.configuration["global"]["scene_path"] == "bistro/Bistro_Exterior.bin"]
+def group_by(values, key):
+    res = {};
+    for v in values:
+        k = key(v)
+        if k in res:
+            res[k].append(v)
+        else:
+            res[k] = [v]
+    return res
 
 class Attenuation:
     def __init__(self, configuration):
@@ -47,48 +40,107 @@ class Lighting:
     def __hash__(self):
         return hash((self.count, self.attenuation))
 
-lightings = sorted({ Lighting(profile.configuration) for profile in current_profiles }, key = lambda v: v.count);
+class Profile:
+    def __init__(self, directory):
+        self.directory = directory
+        self.configuration = toml.load(os.path.join(directory, "configuration.toml"))
+        self.samples = ProfileSamples(directory)
 
-fig, axes = plt.subplots(1, len(lightings), sharey = 'row', squeeze=False)
+def load_profiles(profiling_directory, regex):
+    with os.scandir(profiling_directory) as it:
+        directories = [os.path.join(profiling_directory, entry.name) for entry in it if entry.is_dir() and regex.match(entry.name)]
 
-for col, lighting in enumerate(lightings):
-    current_lighting_profiles = [p for p in current_profiles if lighting == Lighting(p.configuration)]
+    return [Profile(directory) for directory in directories]
 
-    ax = axes[0, col]
-    ax.set_title("Bistro {} lights i = {}, r1 = {:.2f}".format(
-        lighting.count,
-        lighting.attenuation.i,
-        lighting.attenuation.r1
-    ))
+profile_dir_regex = re.compile(r"^(suntem|bistro)_[ \d]{7}_(ortho|persp)_[ \d]{4}$");
+profiles_0 = load_profiles("../profiling", profile_dir_regex);
 
-    ortho_profiles = [p for p in current_lighting_profiles if p.configuration["clustered_light_shading"]["projection"] == "Orthographic"]
+profiles_1 = [profile for profile in profiles_0 if
+                    profile.configuration["global"]["scene_path"] == "bistro/Bistro_Exterior.bin"]
 
-    ortho_profiles.sort(key = lambda p: p.configuration["clustered_light_shading"]["orthographic_sides"]["x"])
+sample_names = ["/frame", "/frame/cluster", "/frame/basic"]
+lightings = sorted({ Lighting(profile.configuration) for profile in profiles_1 }, key = lambda x: x.count)
 
-    for profile in ortho_profiles:
-        frame_sample_index = profile.samples.sample_names.index("/frame")
-        # GPU Samples from all frames except the first
-        run_samples = profile.samples.deltas[1:, :, frame_sample_index, 1]
-        samples = np.nanmin(run_samples, axis = 0) / 1000000.0
-        size = profile.configuration["clustered_light_shading"]["orthographic_sides"]
-        size_x = size["x"]
-        assert size_x == size["y"]
-        assert size_x == size["z"]
-        ax.plot(samples, label="ortho {}".format(size_x))
+# ortho
 
-    persp_profiles = [p for p in current_lighting_profiles if p.configuration["clustered_light_shading"]["projection"] == "Perspective"]
+fig, axes = plt.subplots(len(sample_names), len(lightings), sharex = 'col', squeeze=False, figsize = (thesis.textwidth_inch, thesis.textwidth_inch))
 
-    persp_profiles.sort(key = lambda p: p.configuration["clustered_light_shading"]["perspective_pixels"]["x"])
-    for profile in persp_profiles:
-        frame_sample_index = profile.samples.sample_names.index("/frame")
-        # GPU Samples from all frames except the first
-        run_samples = profile.samples.deltas[1:, :, frame_sample_index, 1]
-        samples = np.nanmin(run_samples, axis = 0) / 1000000.0
-        size = profile.configuration["clustered_light_shading"]["perspective_pixels"]
-        size_x = size["x"]
-        assert size_x == size["y"]
-        ax.plot(samples, label="persp {}".format(size_x))
+for row, sample_name in enumerate(sample_names):
+    for col, lighting in enumerate(lightings):
+        ax = axes[row, col]
+        ax.set_title("{} lights(i = {}, r1 = {:.2f})".format(
+            lighting.count,
+            lighting.attenuation.i,
+            lighting.attenuation.r1
+        ))
 
-    ax.legend()
+        if col == 0:
+            ax.set_ylabel("{} (ms)".format(sample_name))
+
+        if row + 1 == len(sample_names):
+            ax.set_xlabel("frame")
+
+        profiles_2 = sorted([
+            profile for profile in profiles_1
+            if lighting == Lighting(profile.configuration)
+            and profile.configuration["clustered_light_shading"]["projection"] == "Orthographic"
+        ], key = lambda profile: profile.configuration["clustered_light_shading"]["orthographic_sides"]["x"])
+
+        for profile in profiles_2:
+            frame_sample_index = profile.samples.sample_names.index(sample_name)
+            # GPU Samples from all frames except the first
+            run_samples = profile.samples.deltas[1:, :, frame_sample_index, 1]
+            samples = np.nanmin(run_samples, axis = 0) / 1000000.0
+            size = profile.configuration["clustered_light_shading"]["orthographic_sides"]
+            size_x = size["x"]
+            assert size_x == size["y"]
+            assert size_x == size["z"]
+            ax.plot(samples, label="ortho {}".format(size_x))
+
+        ax.legend(loc = 'upper left')
+
+fig.savefig('../media/tune_ortho.pdf', format='pdf', bbox_inches='tight')
+
+# persp
+
+fig, axes = plt.subplots(len(sample_names), len(lightings), sharex = 'col', squeeze=False, figsize = (thesis.textwidth_inch, thesis.textwidth_inch))
+
+for row, sample_name in enumerate(sample_names):
+    for col, lighting in enumerate(lightings):
+        ax = axes[row, col]
+        ax.set_title("{} lights(i = {}, r1 = {:.2f})".format(
+            lighting.count,
+            lighting.attenuation.i,
+            lighting.attenuation.r1
+        ))
+
+        if col == 0:
+            ax.set_ylabel("{} (ms)".format(sample_name))
+
+        if row + 1 == len(sample_names):
+            ax.set_xlabel("frame")
+
+        profiles_2 = sorted([
+            profile for profile in profiles_1
+            if lighting == Lighting(profile.configuration)
+            and profile.configuration["clustered_light_shading"]["projection"] == "Perspective"
+        ], key = lambda p: p.configuration["clustered_light_shading"]["perspective_pixels"]["x"])
+
+        for profile in profiles_2:
+            frame_sample_index = profile.samples.sample_names.index(sample_name)
+            # GPU Samples from all frames except the first
+            run_samples = profile.samples.deltas[1:, :, frame_sample_index, 1]
+            samples = np.nanmin(run_samples, axis = 0) / 1000000.0
+            size = profile.configuration["clustered_light_shading"]["perspective_pixels"]
+            size_x = size["x"]
+            assert size_x == size["y"]
+            ax.plot(samples, label="persp {}".format(size_x))
+
+        ax.legend(loc = 'upper left')
+
+fig.savefig('../media/tune_persp.pdf', format='pdf', bbox_inches='tight')
 
 plt.show()
+
+# 1. for ortho, persp: select a size based on frame time
+# 2. compare best persp and best ortho in (clustering vs shading time), cluster count
