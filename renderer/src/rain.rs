009ff53e60d5535b6c1cc8018177282d722a1bd2
Mick van Gelderen <mickvangelderen@gmail.com>
2019-12-11 13:36:16 +0100

    Spawn all particles immediately

--- renderer/src/rain.rs
+++ renderer/src/rain.rs
@@ -8,9 +8,9 @@ pub struct Particle {
 
 impl Particle {
     #[inline]
-    pub fn new(rng: &mut impl Rng, p0: Point3<f32>, p1: Point3<f32>) -> Self {
+    pub fn spawn(rng: &mut impl Rng, p0: Point3<f32>, p1: Point3<f32>) -> Self {
         Self {
-            position: Vector3::new(rng.gen_range(p0.x, p1.x), p1.y, rng.gen_range(p0.z, p1.z)),
+            position: Vector3::new(rng.gen_range(p0.x, p1.x), rng.gen_range(p0.y, p1.y), rng.gen_range(p0.z, p1.z)),
             velocity: Vector3::new(
                 rng.gen_range(0.2, 1.2),
                 rng.gen_range(-20.0, -14.0),
@@ -24,11 +24,12 @@ impl Particle {
         }
     }
 
+
     #[inline]
     pub fn update(&mut self, delta_time: f32, rng: &mut impl Rng, p0: Point3<f32>, p1: Point3<f32>) {
         self.position += delta_time * self.velocity;
         if self.position.y < p0.y {
-            *self = Particle::new(rng, p0, p1);
+            *self = Particle::spawn(rng, p0, p1);
         }
     }
 }
