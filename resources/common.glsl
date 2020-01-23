e14f3a3d1ed35bca48378aeb7690fcc436e22dbb
Mick van Gelderen <mickvangelderen@gmail.com>
2020-01-23 13:44:02 +0100

    Add byte packing and unpacking functions

--- resources/common.glsl
+++ resources/common.glsl
@@ -26,3 +26,36 @@ float lerp_f32_f32(float x, float x0, float x1, float y0, float y1) {
 float lerp_u32_f32(uint x, uint x0, uint x1, float y0, float y1) {
   return (y1*float(x - x0) + y0*float(x1 - x))/float(x1 - x0);
 }
+
+uint u8x4_to_u32(uvec4 n) {
+  return (n.x & 0xFF) | ((n.y & 0xFF) << 8) | ((n.z & 0xFF) << 16) | ((n.w & 0xFF) << 24);
+}
+
+uint u8x4_to_u32_unsafe(uvec4 n) {
+  return n.x | (n.y << 8) | (n.z << 16) | (n.w << 24);
+}
+
+uvec4 u32_to_u8x4(uint n) {
+  return uvec4(
+    n & 0xFF,
+    (n >> 8) & 0xFF,
+    (n >> 16) & 0xFF,
+    n >> 24
+  );
+}
+
+uint u8x3_to_u32(uvec3 n) {
+  return (n.x & 0xFF) | ((n.y & 0xFF) << 8) | ((n.z & 0xFF) << 16);
+}
+
+uint u8x3_to_u32_unsafe(uvec3 n) {
+  return n.x | (n.y << 8) | (n.z << 16);
+}
+
+uvec3 u32_to_u8x3(uint n) {
+  return uvec3(
+    n & 0xFF,
+    (n >> 8) & 0xFF,
+    (n >> 16) & 0xFF
+  );
+}
