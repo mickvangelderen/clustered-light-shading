6d6278d7bbd8ce8e989c71239b59367fd168e8f0
Mick van Gelderen <mickvangelderen@gmail.com>
2020-01-27 14:19:25 +0100

    Tune VPL

--- resources/light_buffer.glsl
+++ resources/light_buffer.glsl
@@ -2,7 +2,8 @@
 
 layout(std430, binding = LIGHT_BUFFER_BINDING) buffer LightBuffer {
   uint light_count;
-  uint _pad0[15];
+  uint virtual_light_count;
+  uint _pad0[14];
 
   PointLight point_lights[];
 } light_buffer;
